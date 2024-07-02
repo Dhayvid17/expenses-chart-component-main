5955fcc285fff9f1b1c1ba6f7ce11153aa30eba9
dami <dhayvid17@gmail.com>
2024-07-02 17:57:42 +0100

    expenses-chart-component-main

---
+++ expenses-chart-component-main/sandbox.js
@@ -0,0 +1,65 @@
+const bars = document.querySelectorAll(".mon-bar, .tue-bar, .wed-bar, .thur-bar, .fri-bar, .sat-bar, .sun-bar");
+const paras = document.querySelectorAll(".days-bar p");
+
+bars[0].addEventListener("click", function () {
+
+    if (paras[0].style.display === "none") {
+        paras[0].style.display = "block";
+    } else {
+        paras[0].style.display = "none";
+    }
+});
+
+bars[1].addEventListener("click", function () {
+
+    if (paras[1].style.display === "none") {
+        paras[1].style.display = "block";
+    } else {
+        paras[1].style.display = "none";
+    }
+});
+
+bars[2].addEventListener("click", function () {
+
+    if (paras[2].style.display === "none") {
+        paras[2].style.display = "block";
+    } else {
+        paras[2].style.display = "none";
+    }
+});
+
+bars[3].addEventListener("click", function () {
+
+    if (paras[3].style.display === "none") {
+        paras[3].style.display = "block";
+    } else {
+        paras[3].style.display = "none";
+    }
+});
+
+bars[4].addEventListener("click", function () {
+
+    if (paras[4].style.display === "none") {
+        paras[4].style.display = "block";
+    } else {
+        paras[4].style.display = "none";
+    }
+});
+
+bars[5].addEventListener("click", function () {
+
+    if (paras[5].style.display === "none") {
+        paras[5].style.display = "block";
+    } else {
+        paras[5].style.display = "none";
+    }
+});
+
+bars[6].addEventListener("click", function () {
+
+    if (paras[6].style.display === "none") {
+        paras[6].style.display = "block";
+    } else {
+        paras[6].style.display = "none";
+    }
+});
